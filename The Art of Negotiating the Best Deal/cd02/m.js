4e10fca3fb8165529777afa1745f24ae2332e3e5
littleflute <13699175164@sina.cn>
2017-07-15 06:30:22 +0800

    Add files via upload

---
+++ The Art of Negotiating the Best Deal/cd02/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 The Art of Skilled Listening 1.mp3";		
+f[2]	= "02 The Art of Skilled Listening 2.mp3";		
+f[3]	= "03 The Art of Skilled Listening 3.mp3";		
+f[4]	= "04 The Art of Skilled Listening 4.mp3";		
+f[5]	= "05 The Art of Skilled Listening 5.mp3";		
+f[6]	= "06 The Art of Skilled Listening 6.mp3";		
+f[7]	= "07 Knowledge is Power 1.mp3";		
+f[8]	= "08 Knowledge is Power 2.mp3";		
+f[9]	= "09 Knowledge is Power 3.mp3";		
+f[10]	= "10 Knowledge is Power 4.mp3";		
+f[11]	= "11 Knowledge is Power 5.mp3";		
+f[12]	= "12 Knowledge is Power 6.mp3";		
