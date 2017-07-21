3d66ea06faba1521f012c6b0e79c55b7e7afd1bc
littleflute <13699175164@sina.cn>
2017-07-21 13:35:42 -0500

    Add files via upload

---
+++ The Art of Negotiating the Best Deal/cd07/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Hidden FActors That Shape Negotiation 1.mp3";		
+f[2]	= "02 Hidden FActors That Shape Negotiation 2.mp3";		
+f[3]	= "03 Hidden FActors That Shape Negotiation 3.mp3";		
+f[4]	= "04 Hidden FActors That Shape Negotiation 4.mp3";		
+f[5]	= "05 Hidden FActors That Shape Negotiation 5.mp3";		
+f[6]	= "06 Hidden FActors That Shape Negotiation 6.mp3";		
+f[7]	= "07 The Power of Preparation 1.mp3";		
+f[8]	= "08 The Power of Preparation 2.mp3";		
+f[9]	= "09 The Power of Preparation 3.mp3";		
+f[10]	= "10 The Power of Preparation 4.mp3";		
+f[11]	= "11 The Power of Preparation 5.mp3";		
+f[12]	= "12 The Power of Preparation 6.mp3";		
